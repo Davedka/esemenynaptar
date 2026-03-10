df9165b5d707c0eb66744119fd2f92262b85d30d
Dávid <149328692+Davedka@users.noreply.github.com>
2026-03-10 10:49:36 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 ob_start();
 require "config.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->execute([$_POST["username"]]);
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$_POST["email"]]);
     $user = $stmt->fetch();
     
     if ($user && password_verify($_POST["password"], $user["password_hash"])) {
         $_SESSION["user_id"] = $user["id"];
-        $_SESSION["username"] = $user["username"];
+        $_SESSION["fullname"] = $user["fullname"];
         header("Location: dashboard.php");
         exit;
     } else {
@@ -16,27 +16,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
 <link rel="stylesheet" href="style.css">
-
 <div class="container">
     <h2>Bejelentkezés</h2>
-
-    <?php if(isset($error)) echo "<p>$error</p>"; ?>
-
+    <?php if(isset($error)) echo "<p style='color:red'>$error</p>"; ?>
     <form method="post">
-        <input name="username" placeholder="Felhasználónév" required>
+        <input name="email" placeholder="Email" required>
         <input type="password" name="password" placeholder="Jelszó" required>
         <button>Belépés</button>
     </form>
-
     <p style="text-align:center;margin-top:15px;">
         Nincs fiókod? <a href="register.php">Regisztráció</a>
     </p>
-
 </div>
-
-
-
-
-
