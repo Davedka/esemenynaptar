e38817a8af6cf67b5ba9dbf7da071fdabc8c519c
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-10 01:23:27 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
-
 $host = "aws-0-eu-central-1.pooler.supabase.com";
-$port = "6543"; // fontos! pooling port
-$db   = "postgres";
+$port = "6543";
+$dbname = "postgres";
 $user = "postgres.aywstgoezgowqqkrjoqq";
-$pass = "A_JELSZAVAD";
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$db";
-
+$password = "Gazitd0710";
 try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+    echo "Sikeres kapcsolat!";
 } catch (PDOException $e) {
-    die("Adatbázis kapcsolat hiba: " . $e->getMessage());
+    echo "Hiba: " . $e->getMessage();
 }
-
-?>
-
 
