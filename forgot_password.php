74da0bc2e57e8c259415309704dc40eedc1d81f0
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-11 07:53:32 +0100

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -1,5 +1,8 @@
 <?php
 require "config.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require "vendor/autoload.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
@@ -16,9 +19,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $pdo->prepare("UPDATE users SET reset_token = ?, reset_expires = ? WHERE id = ?")
             ->execute([$token, $expires, $user["id"]]);
 
-        // Link kiírása (email küldés helyett, mivel nincs SMTP beállítva)
         $resetLink = "http://" . $_SERVER["HTTP_HOST"] . "/reset_password.php?token=" . $token;
-        $success = $resetLink;
+
+        // Email küldés PHPMailer-rel
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host       = "smtp.gmail.com";      // SMTP szerver
+            $mail->SMTPAuth   = true;
+            $mail->Username   = "te_emailed@gmail.com"; // ← cseréld ki
+            $mail->Password   = "app_jelszo";           // ← cseréld ki (lásd lent)
+            $mail->SMTPSecure = "tls";
+            $mail->Port       = 587;
+
+            $mail->setFrom("te_emailed@gmail.com", "MSZC Eseménynaptár");
+            $mail->addAddress($user["email"], $user["fullname"]);
+
+            $mail->isHTML(true);
+            $mail->Subject = "Jelszó visszaállítás";
+            $mail->Body    = "
+                <h3>Jelszó visszaállítás</h3>
+                <p>Kattints az alábbi linkre a jelszavad visszaállításához:</p>
+                <a href='$resetLink'>$resetLink</a>
+                <p>A link 1 óráig érvényes.</p>
+                <p>Ha nem te kérted, hagyd figyelmen kívül ezt az emailt.</p>
+            ";
+
+            $mail->send();
+            $success = "Az emailt elküldtük a(z) $email címre!";
+
+        } catch (Exception $e) {
+            $error = "Email küldési hiba: " . $mail->ErrorInfo;
+        }
+
     } else {
         $error = "Nem található ilyen email cím!";
     }
@@ -28,10 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="container">
     <h2>Elfelejtett jelszó</h2>
     <?php if(isset($error)) echo "<p style='color:red'>$error</p>"; ?>
-    <?php if(isset($success)): ?>
-        <p style="color:green">Jelszó visszaállító link:</p>
-        <a href="<?= $success ?>"><?= $success ?></a>
-    <?php endif; ?>
+    <?php if(isset($success)) echo "<p style='color:green'>$success</p>"; ?>
     <form method="post">
         <input type="email" name="email" placeholder="Email cím" required>
         <button>Link küldése</button>
