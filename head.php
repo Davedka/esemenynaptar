b4d042ab8ed8da3fbc9a76ebd57d201745feeb35
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-27 17:12:54 +0100

    Update head.php

--- head.php
+++ head.php
@@ -2,7 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Eseménynaptár</title>
 <!-- Favicon – több formátum a jobb kompatibilitásért -->
-<link rel="icon"       href="/favicon.ico"                type="image/x-icon">
 <link rel="icon"       href="/favicon.png"                type="image/png" sizes="64x64">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/favicon.png">
