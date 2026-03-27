077bfa341d4b0288c83422dde2a4710dac98a8a0
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-27 16:23:15 +0100

    Update config.php

--- config.php
+++ config.php
@@ -19,6 +19,13 @@ try {
     die("Adatbázis hiba: " . $e->getMessage());
 }
 
+<head>
+<meta charset="UTF-8">
+<title>Eseménynaptár</title>
+
+<link rel="icon" type="image/png" href="/favicon.png">
+<link rel="stylesheet" href="/style.css">
+</head>
 
 
 
