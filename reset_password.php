19626066f102ad5e23682f64b49028ec0ea851f8
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-27 16:33:14 +0100

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -38,6 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<head>
+<?php include "head.php"; ?>
+</head>
 <link rel="stylesheet" href="style.css">
 <div class="container">
     <h2>Új jelszó beállítása</h2>
