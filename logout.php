727fe11905c4828a688357e37d347f867a36aafa
YTTamas210 <144210517+YTTamas210@users.noreply.github.com>
2026-03-31 09:32:19 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,13 @@
 <?php
-session_start();
+require "config.php";
+
+// Remember me cookie törlése
+if (isset($_COOKIE["remember_token"])) {
+    $pdo->prepare("UPDATE users SET remember_token = NULL, remember_expires = NULL WHERE remember_token = ?")
+        ->execute([$_COOKIE["remember_token"]]);
+    setcookie("remember_token", "", time() - 3600, "/");
+}
+
 session_destroy();
 header("Location: login.php");
+exit;
